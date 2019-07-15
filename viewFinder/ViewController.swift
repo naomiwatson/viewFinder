fbfaefa8d96c7b52dcbb76a7fe4069d25c47d0f8
Naomi Watson <naomiwatson@Naomis-Air.attlocal.net>
2019-07-15 12:35:49 -0700

    initial commit

---
+++ viewFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  viewFinder
+//
+//  Created by Naomi Watson on 7/15/19.
+//  Copyright © 2019 Naomi Watson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
